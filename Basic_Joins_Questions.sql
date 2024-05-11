2e41e6f24260ae2103c91c007fcbc4c9feab9531
Aayushi Tomar <97622835+tomaraayushi@users.noreply.github.com>
2024-05-11 23:09:04 +0200

    Add files via upload

---
+++ Basic_Joins_Questions.sql
@@ -0,0 +1,79 @@
+--LEETCODE INTERVIEW QUESTIONS SERIES
+--BASIC JOINS
+--Q1 
+SELECT
+    CASE
+        WHEN COUNT(u.unique_id) OVER(PARTITION BY u.unique_id) > 1 THEN NULL
+        ELSE u.unique_id 
+    END AS unique_id,
+    e.name 
+FROM Employees e
+LEFT JOIN EmployeeUNI u
+    ON e.id = u.id;
+
+--Q2
+SELECT
+    p.product_name,
+    s.year,
+    s.price
+FROM Product p
+RIGHT JOIN Sales s
+ON p.product_id = s.product_id;
+
+--Q3
+SELECT
+    v.customer_id,
+    COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
+
+--Q4
+SELECT
+    w1.id
+FROM Weather w1
+INNER JOIN Weather w2 
+    ON w1.recordDate = w2.recordDate + INTERVAL'1 day'
+WHERE w1.temperature > w2.temperature;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
